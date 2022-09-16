86e7ef09245a1c69357cf2f34f548af3d7f70e75
Matt Muoio <mmuoio@gmail.com>
2022-09-16 12:54:28 -0400

    v1.0 is complete (or mostly complete)

--- scripts.js
+++ scripts.js
@@ -30,3 +30,21 @@ function growShrinkHeader() {
 
 	//console.log(window.scrollY, introHeight - navHeight);
 }
+
+//hover over .project, reduce opacity of image to 0, and increase opacity of links to 1, animate
+$$(".project").forEach(function (project) {
+	if (window.innerWidth > 750) {
+		project.addEventListener("mouseenter", function () {
+			let image = project.querySelector(".project-image");
+			let links = project.querySelector(".project-links");
+			image.classList.replace("show", "hide30");
+			links.classList.replace("hide", "show");
+		});
+		project.addEventListener("mouseleave", function () {
+			let image = project.querySelector(".project-image");
+			let links = project.querySelector(".project-links");
+			image.classList.replace("hide30", "show");
+			links.classList.replace("show", "hide");
+		});
+	}
+});
